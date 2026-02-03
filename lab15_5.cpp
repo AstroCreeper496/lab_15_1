b09190e80db17dfc2aa79cd4c8287a16af1c3351
AstroCreeper496 <pluccoqlu.1927@gmail.com>
2026-02-03 21:05:19 +0700

    Update lab15_5.cpp

--- lab15_5.cpp
+++ lab15_5.cpp
@@ -20,3 +20,27 @@ int main(){
 
 //Do not modify the code above this line
 //Write definition of shuffle() using pointer after this line. 
+
+void shuffle(int *m,int *n,int *o,int *p) {
+    int a[4];
+    int b[4] = {0,0,0,0};
+    
+    for(int i = 0; i < 4; i++){
+        a[i] = rand();
+        for(int j = 0; j < i; j++){
+            while(a[i] == a[j]) a[j] = rand();
+    }}
+        
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+            if(a[j]>a[i]) b[i]++; 
+        }
+    }
+    
+    int buffer[4] = {*m,*n,*o,*p};
+    *m = buffer[b[0]];
+    *n = buffer[b[1]];
+    *o = buffer[b[2]];
+    *p = buffer[b[3]];
+
+}
